109a066a0347049bc391c03221172e20a2ae4988
Clay McCullough <cmckilla44@gmail.com>
2024-05-11 06:07:23 -0500

    prepare for rebase

--- pyserver/app/agents/__init__.py
+++ pyserver/app/agents/__init__.py
@@ -1,6 +1,22 @@
-from .tools_agent import get_tools_agent_executor
-from .chatbot import get_chatbot_executor
-from .configurable_agent import get_agent_executor, agent, chatbot, chat_retrieval, get_chatbot, LLMType, ConfigurableRetrieval, ConfigurableChatBot, AgentType
-from .retrieval import get_retrieval_executor
-from .tools import RETRIEVAL_DESCRIPTION, TOOLS, AvailableTools, get_retrieval_tool, get_retriever
-from .xml_agent import get_xml_agent_executor
+from pyserver.app.agents.chatbot import get_chatbot_executor
+from pyserver.app.agents.configurable_agent import (
+    AgentType,
+    ConfigurableChatBot,
+    ConfigurableRetrieval,
+    LLMType,
+    agent,
+    chat_retrieval,
+    chatbot,
+    get_agent_executor,
+    get_chatbot,
+)
+# from pyserver.app.agents.openai_agent import get_openai_agent_executor
+from pyserver.app.agents.retrieval import get_retrieval_executor
+from pyserver.app.agents.tools import (
+    RETRIEVAL_DESCRIPTION,
+    TOOLS,
+    AvailableTools,
+    get_retrieval_tool,
+    get_retriever,
+)
+from pyserver.app.agents.xml_agent import get_xml_agent_executor
---
+++ pyserver/app/utils/tests/unit/test_format_docs.py
@@ -0,0 +1,6 @@
+import pytest
+from pyserver.app.utils.format_docs import format_docs
+
+
+def test__format_docs__handles_no_documents():
+    assert True
