5c9608ef96f50b4adc6cd95797d94484111656c1
Kris Hokr <kristoferhokr@gmail.com>
2024-05-14 18:55:33 -0700

    fixed browserQueryClient

--- ui/src/contexts/providers.tsx
+++ ui/src/contexts/providers.tsx
@@ -1,18 +1,32 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
+
+
+function createQueryClient() {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
+        },
+      },
+    })
+  }
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+function getQueryClient() {
+    if (typeof window === 'undefined') {
+        return createQueryClient()
+    }
+
+    if (!browserQueryClient) browserQueryClient = createQueryClient()
+    return browserQueryClient
+}
 
 export default function Providers({children}: {children: ReactNode}) {
-
-    const [queryClient] = useState(() => new QueryClient({
-        defaultOptions: {
-            queries: {
-                // Is this really necessary?
-                staleTime: 60 * 1000
-            }
-        }
-    }))
+   const queryClient = getQueryClient()
 
     return <QueryClientProvider client={queryClient}>
         {children}
