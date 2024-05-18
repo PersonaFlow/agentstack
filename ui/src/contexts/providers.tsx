730ef41a585f8bc648111c8f574d6ca02fcc7339
Kris Hokr <kristoferhokr@gmail.com>
2024-05-17 21:32:24 -0700

    removed unnecessary extra QueryClient creation in provider

--- ui/src/contexts/providers.tsx
+++ ui/src/contexts/providers.tsx
@@ -1,37 +1,22 @@
 'use client'
 
-import { QueryClient, QueryClientProvider, HydrationBoundary } from '@tanstack/react-query'
-import { ReactNode } from 'react'
-
-
-function createQueryClient() {
-    return new QueryClient({
-      defaultOptions: {
-        queries: {
-          staleTime: 60 * 1000,
-        },
-      },
-    })
-  }
-
-let browserQueryClient: QueryClient | undefined = undefined
-
-function getQueryClient() {
-    if (typeof window === 'undefined') {
-        return createQueryClient()
-    }
-
-    if (!browserQueryClient) browserQueryClient = createQueryClient()
-    return browserQueryClient
-}
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactNode, useState } from 'react'
 
 export default function Providers({children}: {children: ReactNode}) {
-   const queryClient = getQueryClient()
-
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
     return <QueryClientProvider client={queryClient}>
-        {/* Might have to redeclare in each server component. */}
-        <HydrationBoundary>
         {children}
-        </HydrationBoundary>
     </QueryClientProvider>
 }
