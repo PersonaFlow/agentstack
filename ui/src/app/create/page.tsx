7c522cf83efbb0bbb3177cd33140e560ff9521e8
Kris Hokr <kristoferhokr@gmail.com>
2024-05-25 10:49:18 -0700

    installed label, textarea

--- ui/src/app/create/page.tsx
+++ ui/src/app/create/page.tsx
@@ -1,19 +1,49 @@
-'use client'
+"use client";
 
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Page() {
-    return <div className="p-6 border-solid border-2 w-full">
-        <h1 className="">Create Assistant</h1>
-        <Select>
-        <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Theme" />
-        </SelectTrigger>
-        <SelectContent>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
-        </SelectContent>
-        </Select>
-    </div>;
+  return (
+    <div className="p-6 border-solid border-2 w-full">
+      <h1>Create Assistant</h1>
+      <div className="my-6 gap-6 flex flex-col">
+        <div className="flex flex-col gap-2">
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Model" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="model-one">Model one</SelectItem>
+              <SelectItem value="model-two">Model two</SelectItem>
+              <SelectItem value="model-three">Model three</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div>
+          <Label htmlFor="preamble">Preamble</Label>
+          <Textarea
+            className="w-[400px]"
+            placeholder="A string to override the preamble."
+            id="preamble"
+          />
+        </div>
+      </div>
+      <div>
+        <Label htmlFor="preamble">Files</Label>
+        <Textarea
+          className="w-[400px]"
+          placeholder="A string to override the preamble."
+          id="preamble"
+        />
+      </div>
+    </div>
+  );
 }
