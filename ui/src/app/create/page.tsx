7293520b8f5849572fc8c34bae8aafd7be9384d8
Kris Hokr <kristoferhokr@gmail.com>
2024-05-27 16:57:53 -0600

    fixed zod schema

--- ui/src/app/create/page.tsx
+++ ui/src/app/create/page.tsx
@@ -29,13 +29,17 @@ import { Button } from "@/components/ui/button";
 const formSchema = z.object({
   public: z.boolean(),
   name: z.string(),
-  interrupt_before_action: z.boolean(),
-  type: z.string(),
-  agent_type: z.string(),
-  llm_type: z.string(),
-  retrieval_description: z.string(),
-  system_message: z.string(),
-  files: z.array(z.string()),
+  config: z.object({
+    configurable: z.object({
+      interrupt_before_action: z.boolean(),
+      type: z.string(),
+      agent_type: z.string(),
+      llm_type: z.string(),
+      retrieval_description: z.string(),
+      system_message: z.string(),
+    }),
+  }),
+  file_ids: z.array(z.string()),
 });
 
 export default function Page() {
@@ -46,20 +50,22 @@ export default function Page() {
     defaultValues: {
       public: false,
       name: "",
-      interrupt_before_action: false,
-      type: "agent",
-      agent_type: "GPT 3.5 Turbo",
-      llm_type: "GPT 3.5 Turbo",
-      retrieval_description:
-        "Can be used to look up information that was uploaded for this assistant.",
-      system_message: "You are a helpful assistant.",
-      files: [],
+      config: {
+        configurable: {
+          interrupt_before_action: false,
+          type: "agent",
+          agent_type: "GPT 3.5 Turbo",
+          llm_type: "GPT 3.5 Turbo",
+          retrieval_description:
+            "Can be used to look up information that was uploaded for this assistant.",
+          system_message: "You are a helpful assistant.",
+        },
+      },
+      file_ids: [],
     },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
@@ -67,128 +73,212 @@ export default function Page() {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="p-6 border-solid border-2 w-full"
+        className="p-6 border-solid border-2 w-full gap-4 flex flex-col"
       >
-        <div className="flex gap-8 align-center">
-          <h1>Create Assistant</h1>
-          <div className="flex items-center gap-2">
+        <h1>Create Assistant</h1>
+        <div className="flex gap-12 items-center">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Assistant Name"
+                    type="text"
+                    {...field}
+                    className="w-[400px]"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="public"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Public</FormLabel>
+                <FormControl>
+                  <Switch
+                    onCheckedChange={(checked) => field.onChange(checked)}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex gap-6">
+          <FormField
+            control={form.control}
+            name="config.configurable.type"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Bot Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="chatbot">Chatbot</SelectItem>
+                      <SelectItem value="chat_retrieval">
+                        Chat Retrieval
+                      </SelectItem>
+                      <SelectItem value="agent">Agent</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="config.configurable.agent_type"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Agent Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="GPT 3.5 Turbo">
+                        GPT 3.5 Turbo
+                      </SelectItem>
+                      <SelectItem value="GPT 4 Turbo">GPT 4 Turbo</SelectItem>
+                      <SelectItem value="GPT 4 (Azure OpenAI)">
+                        GPT 4 (Azure OpenAI)
+                      </SelectItem>
+                      <SelectItem value="Claude 2">Claude 2</SelectItem>
+                      <SelectItem value="Claude 2 (Amazon Bedrock)">
+                        Claude 2 (Amazon Bedrock)
+                      </SelectItem>
+                      <SelectItem value="GEMINI">GEMINI</SelectItem>
+                      <SelectItem value="Ollama">Ollama</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="config.configurable.llm_type"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="LLM Type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="GPT 3.5 Turbo">GPT 3.5 Turbo</SelectItem>
+                    <SelectItem value="GPT 4">GPT 4</SelectItem>
+                    <SelectItem value="GPT 4 (Azure OpenAI)">
+                      GPT 4 (Azure OpenAI)
+                    </SelectItem>
+                    <SelectItem value="Claude 2">Claude 2</SelectItem>
+                    <SelectItem value="Claude 2 (Amazon Bedrock)">
+                      Claude 2 (Amazon Bedrock)
+                    </SelectItem>
+                    <SelectItem value="GEMINI">GEMINI</SelectItem>
+                    <SelectItem value="Ollama">Ollama</SelectItem>
+                    <SelectItem value="Mixtral">Mixtral</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="config.configurable.system_message"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>System Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  className="w-[400px]"
+                  placeholder="Instructions for the assistant. ex: 'You are a helpful assistant'"
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <div className="flex gap-4">
+          {userFiles?.length > 0 && (
             <FormField
               control={form.control}
-              name="username"
+              name="file_ids"
               render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Public</FormLabel>
+                <FormItem className="flex flex-col">
+                  <FormLabel>Retrieval descriptio</FormLabel>
                   <FormControl>
-                    <Switch {...field} />
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Files" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {userFiles?.map((file) => (
+                          <SelectItem value={file.id}>
+                            {file.filename}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
                   </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
                 </FormItem>
               )}
             />
-          </div>
-        </div>
-        <div className="my-6 gap-6 flex flex-col">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Input id="name" type="text" placeholder="Assistant Name" />
-          </div>
-          <div className="flex items-center gap-2">
-            <Label htmlFor="interrupt_before_action">
-              Interrupt before action?
-            </Label>
-            <Switch id="interrupt_before_action" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Bot Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="chatbot">Chatbot</SelectItem>
-                <SelectItem value="chat_retrieval">Chat Retrieval</SelectItem>
-                <SelectItem value="agent">Agent</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex flex-col gap-2">
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Agent Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="GPT 3.5 Turbo">GPT 3.5 Turbo</SelectItem>
-                <SelectItem value="GPT 4 Turbo">GPT 4 Turbo</SelectItem>
-                <SelectItem value="GPT 4 (Azure OpenAI)">
-                  GPT 4 (Azure OpenAI)
-                </SelectItem>
-                <SelectItem value="Claude 2">Claude 2</SelectItem>
-                <SelectItem value="Claude 2 (Amazon Bedrock)">
-                  Claude 2 (Amazon Bedrock)
-                </SelectItem>
-                <SelectItem value="GEMINI">GEMINI</SelectItem>
-                <SelectItem value="Ollama">Ollama</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex flex-col gap-2">
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="LLM Type" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="GPT 3.5 Turbo">GPT 3.5 Turbo</SelectItem>
-                <SelectItem value="GPT 4">GPT 4</SelectItem>
-                <SelectItem value="GPT 4 (Azure OpenAI)">
-                  GPT 4 (Azure OpenAI)
-                </SelectItem>
-                <SelectItem value="Claude 2">Claude 2</SelectItem>
-                <SelectItem value="Claude 2 (Amazon Bedrock)">
-                  Claude 2 (Amazon Bedrock)
-                </SelectItem>
-                <SelectItem value="GEMINI">GEMINI</SelectItem>
-                <SelectItem value="Ollama">Ollama</SelectItem>
-                <SelectItem value="Mixtral">Mixtral</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Label htmlFor="retrieval_description">Retrieval description</Label>
-            <Textarea
-              className="w-[400px]"
-              placeholder="Tool description providing instructions to the LLM for its use"
-              id="retrieval_description"
-            />
-          </div>
-          <div>
-            <Label htmlFor="system_message">System Message</Label>
-            <Textarea
-              className="w-[400px]"
-              placeholder="Instructions for the assistant. ex: 'You are a helpful assistant'"
-              id="system_message"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          {userFiles && (
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Files" />
-              </SelectTrigger>
-              <SelectContent>
-                {userFiles?.map((file) => (
-                  <SelectItem value={file.id}>{file.filename}</SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
           )}
+          {/* <Button className="w-40"> */}
+          {/* Add new file */}
+          {/* <Input type="file" /> */}
+          {/* </Button> */}
         </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="add-file">Add new file</Label>
-          <Input id="picture" type="file" />
-        </div>
-        <Button type="submit">Submit</Button>
+
+        {/* <FormField
+          control={form.control}
+          name="retrieval_description"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Retrieval description</FormLabel>
+              <FormControl>
+                <Textarea
+                  className="w-[400px]"
+                  placeholder="Used to look up information that was uploaded for this assistant."
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className="flex items-center gap-2">
+          <Label htmlFor="interrupt_before_action">
+            Interrupt before action?
+          </Label>
+          <Switch id="interrupt_before_action" />
+        </div> */}
+        <Button type="submit" className="w-40">
+          Submit
+        </Button>
       </form>
     </Form>
   );
