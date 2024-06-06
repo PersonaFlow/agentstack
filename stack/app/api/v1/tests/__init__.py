17415a15bb413dc822aa1b2e7266073e00f56586
Clay McCullough <cmckilla44@gmail.com>
2024-06-06 11:21:06 -0500

    Fix Async testing in Pants (#74)
    * First unit test, test healthcheck endpoint
    
    * Async tests are working now!
    
    * DRY out test deps

---
+++ stack/app/api/v1/tests/__init__.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Response
+
+# from stack.app.api.v1 import feedback
+# from stack.app.api.v1 import chat
+from stack.app.api.v1 import runs
+from stack.app.api.v1 import users
+from stack.app.api.v1 import threads
+from stack.app.api.v1 import messages
+from stack.app.api.v1 import assistants
+from stack.app.api.v1 import rag
+from stack.app.api.v1 import files
+
+api_router = APIRouter()
+# api_router.include_router(feedback.router, tags=["Feedback"])
+# api_router.include_router(chat.router, prefix="/chat", tags=["Chat"])
+api_router.include_router(runs.router, prefix="/runs")
+api_router.include_router(users.router, prefix="/users")
+api_router.include_router(threads.router, prefix="/threads")
+api_router.include_router(messages.router, prefix="/messages")
+api_router.include_router(assistants.router, prefix="/assistants")
+api_router.include_router(rag.router, prefix="/rag")
+api_router.include_router(files.router, prefix="/files")
+
+
+@api_router.get("/health_check", tags=["Health Check"])
+def health_check():
+    raise Exception('asdf 123')
+    return Response(content="BAD", status_code=500)
