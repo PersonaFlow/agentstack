30adbaff600909e5c117e8c339f5d666068c6bf9
Clay McCullough <cmckilla44@gmail.com>
2024-06-07 07:07:50 -0500

    User router unit tests

--- stack/tests/unit/conftest.py
+++ stack/tests/unit/conftest.py
@@ -1,13 +1,44 @@
+import uuid
+from datetime import datetime
+
 import pytest
-from httpx import AsyncClient
 
 from stack.app.app_factory import create_app
 from stack.app.core.configuration import Settings
+from stack.app.model.thread import Thread as ModelThread
+from stack.app.model.user import User as ModelUser
+from stack.app.schema.user import User as SchemaUser
 
 app = create_app(Settings())
 
 
+def passthrough(repository):
+    def wrapper():
+        return repository
+
+    return wrapper
+
+
 @pytest.fixture
-async def client():
-    async with AsyncClient(app=app, base_url="http://localhost:9000") as ac:
-        yield ac
+def random_schema_user() -> SchemaUser:
+    return SchemaUser(
+        user_id=str(uuid.uuid4()), created_at=datetime.now(), updated_at=datetime.now()
+    )
+
+
+@pytest.fixture
+def random_model_user() -> ModelUser:
+    return ModelUser(
+        user_id=str(uuid.uuid4()), created_at=datetime.now(), updated_at=datetime.now()
+    )
+
+
+@pytest.fixture
+def random_model_thread() -> ModelThread:
+    return ModelThread(
+        id=uuid.uuid4(),
+        user_id=str(uuid.uuid4()),
+        assistant_id="asdf",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
