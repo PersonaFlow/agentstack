ce0913bdf6ba3fb8a61e77ecb2d96ed668938f06
Dan Orlando <dan@danorlando.com>
2024-06-08 14:00:27 -0700

    Assistants tests

--- stack/tests/unit/conftest.py
+++ stack/tests/unit/conftest.py
@@ -8,6 +8,9 @@ from stack.app.core.configuration import Settings
 from stack.app.model.thread import Thread as ModelThread
 from stack.app.model.user import User as ModelUser
 from stack.app.schema.user import User as SchemaUser
+from stack.app.schema.assistant import Assistant
+from stack.app.schema.file import FileSchema
+
 
 app = create_app(Settings())
 
@@ -41,4 +44,29 @@ def random_model_thread() -> ModelThread:
         assistant_id="asdf",
         created_at=datetime.now(),
         updated_at=datetime.now(),
+    )
+
+@pytest.fixture
+def random_schema_assistant() -> Assistant:
+    return Assistant(
+        id=uuid.uuid4(),
+        config={"configurable": {}},
+        name="My Assistant",
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
+@pytest.fixture
+def random_schema_file() -> FileSchema:
+    return FileSchema(
+        id=uuid.uuid4(),
+        user_id="test_user",
+        purpose="assistants",
+        filename="my_file.pdf",
+        bytes=123,
+        mime_type="application/pdf",
+        source="files/my_file.pdf",
+        kwargs={},
+        created_at=datetime.now(),
+        updated_at=datetime.now()
     )
