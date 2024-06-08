3245de3cdd318ffb74ff547a797681d90c257ad7
Dan Orlando <dan@danorlando.com>
2024-06-08 15:08:29 -0700

    threads happy path

--- stack/tests/unit/conftest.py
+++ stack/tests/unit/conftest.py
@@ -7,9 +7,11 @@ from stack.app.app_factory import create_app
 from stack.app.core.configuration import Settings
 from stack.app.model.thread import Thread as ModelThread
 from stack.app.model.user import User as ModelUser
+from stack.app.model.message import Message as MessageModel
 from stack.app.schema.user import User as SchemaUser
 from stack.app.schema.assistant import Assistant
 from stack.app.schema.file import FileSchema
+from stack.app.schema.thread import Thread as ThreadSchema
 
 
 app = create_app(Settings())
@@ -39,7 +41,7 @@ def random_model_user() -> ModelUser:
 @pytest.fixture
 def random_model_thread() -> ModelThread:
     return ModelThread(
-        id=uuid.uuid4(),
+        id=str(uuid.uuid4()),
         user_id=str(uuid.uuid4()),
         assistant_id="asdf",
         created_at=datetime.now(),
@@ -49,7 +51,7 @@ def random_model_thread() -> ModelThread:
 @pytest.fixture
 def random_schema_assistant() -> Assistant:
     return Assistant(
-        id=uuid.uuid4(),
+        id=str(uuid.uuid4()),
         config={"configurable": {}},
         name="My Assistant",
         created_at=datetime.now(),
@@ -70,3 +72,30 @@ def random_schema_file() -> FileSchema:
         created_at=datetime.now(),
         updated_at=datetime.now()
     )
+
+@pytest.fixture
+def random_schema_thread() -> ThreadSchema:
+    return ThreadSchema(
+        id=str(uuid.uuid4()),
+        user_id=str(uuid.uuid4()),
+        assistant_id="assistant1",
+        name="Test Thread",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+
+
+@pytest.fixture
+def random_model_message() -> MessageModel:
+    return MessageModel(
+        id=str(uuid.uuid4()),
+        thread_id=str(uuid.uuid4()),
+        user_id=str(uuid.uuid4()),
+        assistant_id=str(uuid.uuid4()),
+        content="Test Message",
+        type="AI",
+        example=False,
+        created_at=datetime.now(),
+        updated_at=datetime.now()
+    )
+
