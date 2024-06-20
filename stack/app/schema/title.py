61c251bab5686d58916fc0356b45d7fbb62e291f
Dan Orlando <dan@danorlando.com>
2024-06-20 16:41:38 -0700

    Enable Observability and Eval via LangFuse and Arize Phoenix (#102)
    * update env's
    
    * update docker compose
    
    * Add langfuse library
    
    * Add callback to runs
    
    * Working implementation
    
    * Add Arize Phoenix library
    
    * Implement Arize-AI Phoenix

--- stack/app/schema/title.py
+++ stack/app/schema/title.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from stack.app.schema.message import Message
 
 
 class Source(BaseModel):
@@ -12,12 +11,15 @@ class Metadata(BaseModel):
     run_id: Optional[str]
     sources: Optional[List[Source]]
 
+class ChatMessage(BaseModel):
+    content: str
+    type: str
 
 class TitleRequest(BaseModel):
     thread_id: str = Field(
         ..., description="The id of the thread to generate the title for."
     )
-    history: Optional[List[Message]] = Field(
+    history: Optional[List[ChatMessage]] = Field(
         None,
         description="(Optional) The conversation history of the thread. This is used as context for the model when generating the title.",
     )
