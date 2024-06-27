2e61d574370d3747dd8266379a67ca58a7e76c1f
Kris Hokr <kristoferhokr@gmail.com>
2024-06-27 17:51:14 -0600

    added config values to edit-assistant

--- ui/src/app/features/build-panel/components/create-assistant.tsx
+++ ui/src/app/features/build-panel/components/create-assistant.tsx
@@ -10,15 +10,11 @@ import { useEffect, useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { AssistantForm } from "./assistant-form";
-import {
-  TAssistant,
-  TConfigSchema,
-  TConfigurableSchema,
-  TSchemaField,
-} from "@/data-provider/types";
+import { TAssistant, TConfigSchema, TSchemaField } from "@/data-provider/types";
 import { useAtom } from "jotai";
 import { assistantAtom } from "@/store";
 import Spinner from "@/components/ui/spinner";
+import { useConfigSchema } from "@/hooks/useConfig";
 
 const formSchema = z.object({
   public: z.boolean(),
@@ -52,40 +48,6 @@ const defaultValues = {
     },
   },
   file_ids: [],
-};
-
-const useConfigSchema = (
-  configSchema: TConfigurableSchema,
-  selectedArchType: string,
-) => {
-  const configProperties =
-    configSchema?.definitions["Configurable" as TConfigurableSchema].properties;
-
-  let systemMessage = "";
-  let retrievalDescription = "";
-
-  if (selectedArchType === "chat_retrieval") {
-    systemMessage =
-      configProperties["type==chat_retrieval/system_message"].default;
-  }
-
-  if (selectedArchType === "chatbot") {
-    systemMessage = configProperties["type==chatbot/system_message"].default;
-  }
-
-  if (selectedArchType === "agent") {
-    systemMessage = configProperties["type==agent/system_message"].default;
-  }
-
-  if (selectedArchType) {
-    retrievalDescription =
-      configProperties["type==agent/retrieval_description"].default;
-  }
-
-  return {
-    systemMessage,
-    retrievalDescription,
-  };
 };
 
 export function CreateAssistant() {
