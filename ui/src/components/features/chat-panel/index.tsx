efba60482a1d8e500cdf58fad813ffdb4db973a8
Kris Hokr <kristoferhokr@gmail.com>
2024-06-27 23:49:10 -0600

    installed @microsoft/fetch-event-source

--- ui/src/components/features/chat-panel/index.tsx
+++ ui/src/components/features/chat-panel/index.tsx
@@ -9,67 +9,8 @@ import { assistantAtom, conversationAtom } from "@/store";
 import { useParams } from "next/navigation";
 import { useToast } from "@/components/ui/use-toast";
 
-const data = {
-  values: [
-    {
-      content: "hello",
-      additional_kwargs: {},
-      response_metadata: {},
-      type: "human",
-      name: null,
-      id: "human-0.98618096892379",
-      example: false,
-    },
-    {
-      content: "Hello! How can I assist you today?",
-      additional_kwargs: {},
-      response_metadata: {
-        finish_reason: "stop",
-      },
-      type: "ai",
-      name: null,
-      id: "run-8ad1bd14-6340-4afw9-b357-32awc210f150f",
-      example: false,
-      tool_calls: [],
-      invalid_tool_calls: [],
-    },
-    {
-      content: "I am working on a project called PersonaFlow.",
-      additional_kwargs: {},
-      response_metadata: {},
-      type: "human",
-      name: null,
-      id: "human-0.9861809w968792379",
-      example: false,
-    },
-    {
-      content: "That's neat! How can I help?",
-      additional_kwargs: {},
-      response_metadata: {
-        finish_reason: "stop",
-      },
-      type: "ai",
-      name: null,
-      id: "run-8ad1bd14-6340-4af9-b357-32wac210f150f",
-      example: false,
-      tool_calls: [],
-      invalid_tool_calls: [],
-    },
-    {
-      content: "Help me understand the meaning of life.",
-      additional_kwargs: {},
-      response_metadata: {},
-      type: "human",
-      name: null,
-      id: "human-0.986109968792379",
-      example: false,
-    },
-  ],
-  next: [],
-};
-
 export default function ChatPanel() {
-  const [conversation] = useAtom(conversationAtom);
+  const [conversation, setConversation] = useAtom(conversationAtom);
   const [assistant] = useAtom(assistantAtom);
   const { id: threadId } = useParams();
   const { data: threadState } = useThreadState(threadId);
