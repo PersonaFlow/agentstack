e34297cfcc48fc81c8e2b52be17f0c6064c46e62
Dan Orlando <dan@danorlando.com>
2024-06-28 10:49:25 -0700

    auth strategies

---
+++ stack/app/core/auth/strategies/base.py
@@ -0,0 +1,79 @@
+from abc import abstractmethod
+from typing import Any, List
+
+
+class BaseAuthenticationStrategy:
+    """
+    Base strategy for authentication, abstract class that should be inherited from.
+
+    Attributes:
+        NAME (str): The name of the strategy.
+    """
+
+    NAME = "Base"
+
+    @staticmethod
+    def get_required_payload(self) -> List[str]:
+        """
+        The required /login payload for the Auth strategy
+        """
+        ...
+
+    @abstractmethod
+    def login(self, **kwargs: Any):
+        """
+        Check email/password credentials and return JWT token.
+        """
+        ...
+
+
+class BaseOAuthStrategy:
+    """
+    Base strategy for OAuth, abstract class that should be inherited from.
+
+    Attributes:
+        NAME (str): The name of the strategy.
+    """
+
+    NAME = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._post_init_check()
+
+    def _post_init_check(self):
+        if any(
+            [
+                self.NAME is None,
+            ]
+        ):
+            raise ValueError(f"{self.__name__} must have NAME parameter(s) defined.")
+
+    @abstractmethod
+    def get_client_id(self, **kwargs: Any):
+        """
+        Retrieves the OAuth app's client ID
+        """
+        ...
+
+    @abstractmethod
+    def get_authorization_endpoint(self, **kwargs: Any):
+        """
+        Retrieves the OAuth app's authorization endpoint.
+        """
+        ...
+
+    @abstractmethod
+    async def get_endpoints(self, **kwargs: Any):
+        """
+        Retrieves the /token and /userinfo endpoints.
+        """
+        ...
+
+    @abstractmethod
+    async def authorize(self, **kwargs: Any):
+        """
+        Authentication logic: dealing with user data and returning it
+        to set the current user session for OAuth strategies.
+        """
+        ...
