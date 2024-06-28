2a1ec517b5694af759a3305703fe6814533879b7
Dan Orlando <dan@danorlando.com>
2024-06-28 10:49:04 -0700

    JWTService, validators, auth config

---
+++ stack/app/core/auth/jwt.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta, timezone
+import structlog
+import uuid
+import jwt
+from app.core.configuration import settings
+
+logger = structlog.get_logger()
+
+
+class JWTService:
+
+    def __init__(self):
+        secret_key = settings.AUTH_SECRET_KEY
+        algorithm = settings.JWT_ALGORITHM
+        expiry = settings.TOKEN_EXPIRY_HOURS
+        issuer = settings.JWT_ISSUER
+
+        if not secret_key:
+            raise ValueError("Secret auth key is not set.")
+
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+        self.expiry = expiry
+        self.issuer = issuer
+
+    def create_and_encode_jwt(self, user: dict) -> str:
+        now = datetime.now(timezone.utc)
+        payload = {
+            "iss": self.issuer,
+            "iat": now,
+            "exp": now + timedelta(hours=self.expiry),
+            "jti": str(uuid.uuid4()),
+            "context": user,
+        }
+
+        token = jwt.encode(payload, self.secret_key, self.algorithm)
+
+        return token
+
+    def decode_jwt(self, token: str) -> dict:
+        try:
+            decoded_payload = jwt.decode(
+                token, self.secret_key, algorithms=[self.algorithm]
+            )
+            return decoded_payload
+        except jwt.ExpiredSignatureError:
+            logger.warning("Token has expired.")
+            return None
+        except jwt.InvalidTokenError:
+            logger.warning("Invalid token.")
+            return None
---
+++ stack/app/core/auth/utils.py
@@ -0,0 +1,51 @@
+from fastapi import Request
+
+from stack.app.core.auth.auth_config import ENABLED_AUTH_STRATEGY_MAPPING, is_authentication_enabled
+
+from stack.app.core.auth.jwt import JWTService
+
+
+def is_enabled_authentication_strategy(strategy_name: str) -> bool:
+    """
+    Check whether a given authentication strategy is enabled in config/auth.py
+
+    Args:
+        strategy_name (str): Name the of auth strategy.
+
+    Returns:
+        bool: Whether that strategy is currently enabled
+    """
+    # Check the strategy is valid and enabled
+    if strategy_name not in ENABLED_AUTH_STRATEGY_MAPPING.keys():
+        return False
+
+    return True
+
+
+
+def get_header_user_id(request: Request) -> str:
+    """
+    Retrieves the user_id from request headers, will work whether authentication is enabled or not.
+
+    (Auth disabled): retrieves the User-Id header value
+    (Auth enabled): retrieves the Authorization header, and decodes the value
+
+    Args:
+        request (Request): current Request
+
+
+    Returns:
+        str: User ID
+    """
+    # Check if Auth enabled
+    if is_authentication_enabled():
+        # Validation already performed, so just retrieve value
+        authorization = request.headers.get("Authorization")
+        _, token = authorization.split(" ")
+        decoded = JWTService().decode_jwt(token)
+
+        return decoded["context"]["id"]
+    # Auth disabled
+    else:
+        user_id = request.headers.get("User-Id", "")
+        return user_id
