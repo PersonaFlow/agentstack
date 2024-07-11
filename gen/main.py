2e40365fc44c383802972a94ad7c648c3559222f
Clay McCullough <cmckilla44@gmail.com>
2024-07-11 06:59:54 -0500

    starting work

--- gen/main.py
+++ gen/main.py
@@ -1,20 +1,31 @@
 import structlog
 
+from gen.schema.schema import Persona, Trait, Configuration
+
 logger = structlog.get_logger()
 
-persona = {
+persona = Persona(
+    name="football coach",
+    traits=[
+        Trait(
+            question="What is your strongest skill?",
+            answer="I can motivate my players to do far beyond what they thought possible"
+        ),
+        Trait(
+            question="What kind of football plays do you like to run?",
+            answer="I prefer to run passing plays. While they are more risky, they have greater potential for reward and don't wear my team out as much."
+        )
+    ]
+)
 
-}
-
-configuration = {
-
-}
+configuration = Configuration()
 
 
 if __name__ == '__main__':
     logger.info('Starting Gen')
 
     # TODO: Step 0, get persona and configuration
+    logger.info(persona.json())
 
     # TODO: Step 1, embed input data
 
--- gen/schema/schema.py
+++ gen/schema/schema.py
@@ -1,64 +1,21 @@
 import uuid
-from typing import List, Protocol
+from typing import List
 
 from pydantic import BaseModel
 
 
-class Fact(BaseModel):
-    id: uuid.UUID
-    name: str
-    version: str
+class Trait(BaseModel):
+    id: uuid.UUID = uuid.uuid4()
     question: str
     answer: str
-    weight: float = 1.0
-
-
-class Trait(BaseModel):
-    id: uuid.UUID
-    name: str
-    version: str
-    facts: List[Fact]
-    weight: float = 1.0
 
 
 class Persona(BaseModel):
-    id: uuid.UUID
+    id: uuid.UUID = uuid.uuid4()
     name: str
     traits: List[Trait]
-    version: str
 
 
 class Configuration(BaseModel):
     # Stub for now
     embed_settings: str = ""
-
-
-class Task(Protocol):
-    configuration: Configuration
-
-    def __init__(self, configuration: Configuration):
-        self.configuration = configuration
-
-    def __call__(self, *args, **kwargs):
-        ...
-
-
-class Flow(Protocol):
-    configuration: Configuration
-    tasks: List[Task]
-
-    def __call__(self, *args, **kwargs):
-        ...
-
-
-class QAFlow(Flow):
-    configuration: Configuration
-    tasks: List[Task]
-
-    def __init__(self, configuration: Configuration, tasks: List[Task]):
-        self.configuration = configuration
-        self.tasks = tasks
-
-    def __call__(self, *args, **kwargs):
-        pass
-
