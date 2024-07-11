8bd64d24641fa50dba1698b2e5dcae782c905647
Clay McCullough <cmckilla44@gmail.com>
2024-07-11 06:31:58 -0500

    add steps

---
+++ gen/main.py
@@ -0,0 +1,28 @@
+import structlog
+
+logger = structlog.get_logger()
+
+persona = {
+
+}
+
+configuration = {
+
+}
+
+
+if __name__ == '__main__':
+    logger.info('Starting Gen')
+
+    # TODO: Step 0, get persona and configuration
+
+    # TODO: Step 1, embed input data
+
+    # TODO: Step 2, Gen questions Persona would ask
+
+    # TODO: Step 3, Gen answers to questions via RAG
+
+    # TODO: Step 4, Eval
+
+    # TODO: Step 5, Output
+
