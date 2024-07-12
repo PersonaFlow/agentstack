3f9fab746e1d5cc51305219451d7a218ac11cbcb
Kris Hokr <kristoferhokr@gmail.com>
2024-07-12 13:31:54 -0600

    removed underline in accordion, added link to visit url

--- ui/src/components/features/tools/tool-result.tsx
+++ ui/src/components/features/tools/tool-result.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import {
   Collapsible,
@@ -6,20 +12,50 @@ import {
 } from "@/components/ui/collapsible";
 import { TMessage, TToolResult } from "@/data-provider/types";
 import { CaretSortIcon } from "@radix-ui/react-icons";
+import { ArrowRight, MoveUpRight } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 
+const AccordionToolContent = ({
+  content,
+  url,
+}: {
+  content: string;
+  url: string;
+}) => {
+  return (
+    <Accordion type="single" collapsible>
+      <AccordionItem value={url}>
+        <AccordionTrigger className="text-left">
+          <div className="flex w-full">
+            {url}
+            <Button variant="outline" type="button" className="ml-auto mr-2">
+              <Link
+                href={url}
+                target="_blank"
+                className="flex gap-2 items-center"
+              >
+                Visit
+                <MoveUpRight className="w-4 h-4" />
+              </Link>
+            </Button>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>{content}</AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
 export function ToolResult({ toolResult }: { toolResult: TMessage }) {
   const [isOpen, setIsOpen] = useState(false);
-
-  console.log(toolResult);
 
   return (
     <div className="flex flex-col py-2 px-3 text-base md:px-4 mr-auto w-3/4 md:px-5 lg:px-1 xl:px-5 flex-col gap-4">
       <Collapsible open={isOpen} onOpenChange={setIsOpen}>
         <div className="flex items-center justify-between">
           <CollapsibleTrigger asChild>
-            <Button variant="ghost" type="button" className="gap-2 mb-2">
+            <Button variant="outline" type="button" className="gap-2 mb-2">
               <span>Tool results: {toolResult.name}</span>
               <CaretSortIcon className="h-5 w-5" />
             </Button>
@@ -28,11 +64,7 @@ export function ToolResult({ toolResult }: { toolResult: TMessage }) {
         <CollapsibleContent className="space-y-2">
           {Array.isArray(toolResult.content)
             ? toolResult.content.map((item) => (
-                <div className="cursor-pointer rounded-md border px-4 py-2 font-mono text-sm shadow-sm hover:bg-stone-400 hover:text-white">
-                  <Link href={item.url} target="_blank">
-                    {item.url}
-                  </Link>
-                </div>
+                <AccordionToolContent url={item.url} content={item.content} />
               ))
             : toolResult.content}
         </CollapsibleContent>
