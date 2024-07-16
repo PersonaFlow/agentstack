2c7761f37b492897123876285a27e01bc1e20350
Kris Hokr <kristoferhokr@gmail.com>
2024-07-16 16:13:47 -0600

    removed unused components

--- ui/src/components/features/markdown/components/tags.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithoutRef } from "react";
-import type {
-  //   Component,
-  ExtraProps,
-} from "hast-util-to-jsx-runtime";
-
-export const P = ({ children }) => {
-  return <p dir="auto">{children}</p>;
-};
