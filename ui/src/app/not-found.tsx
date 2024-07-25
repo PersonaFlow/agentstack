40cccdb10acee30185f98aa6edbacf8f78b88734
Kris Hokr <kristoferhokr@gmail.com>
2024-07-25 11:01:25 -0600

    added not-found page

---
+++ ui/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return <div className="border-solid border-2 w-full gap-4 flex flex-col items-center justify-center">
+
+  <h1>Page not found.</h1>
+  </div>
+};
+
+export default Page;
