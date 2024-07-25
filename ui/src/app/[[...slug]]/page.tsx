52f1c2d45f83b231e98ffd9520b4b6837f4f5f13
Kris Hokr <kristoferhokr@gmail.com>
2024-07-25 11:44:01 -0600

    useSlugParams

--- ui/src/app/[[...slug]]/page.tsx
+++ ui/src/app/[[...slug]]/page.tsx
@@ -1,9 +1,12 @@
+// 'use client'
 import ChatPanel from "@/components/features/chat-panel";
+import { useEffect } from "react";
 
-export default function ConversationPage() {
+export default function ConversationPage({params}: {params: {slug?: string[]}}) {
+
   return (
     <div className="border-solid border-2 w-full gap-4 flex flex-col">
-      <ChatPanel />
+      {/* <ChatPanel /> */}
     </div>
   );
 }
---
+++ ui/src/hooks/useSlugParams.ts
@@ -0,0 +1,24 @@
+import { useParams } from 'next/navigation';
+import { useMemo } from 'react';
+
+
+/**
+ *
+ * @description This hook is used to parse the slug from the URL and return the agentId and conversationId.
+ * The slug can be in the following formats:
+ * - [] - /
+ * - NOT THIS [c, :conversationId] - /c/:conversationId
+ * - [a, :agentId] - /a/:agentId
+ * - [a, :agentId, c, :conversationId] - /a/:agentId/c/:conversationId
+ */
+
+export const useSlugRoutes = () => {
+    const {params} = useParams();
+
+  const { assistantId, threadId } = useMemo(() => {
+    const slug = (params ?? []) as string[];
+    return getSlugRoutes(slug);
+  }, [params]);
+
+  return { assistantId, threadId };
+};
