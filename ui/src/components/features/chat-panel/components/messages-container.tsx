28d9fdafa9892f8e99a9451d428081e0af0131e2
Kris Hokr <kristoferhokr@gmail.com>
2024-08-09 11:27:56 -0600

    display retry

--- ui/src/components/features/chat-panel/components/messages-container.tsx
+++ ui/src/components/features/chat-panel/components/messages-container.tsx
@@ -4,15 +4,13 @@ import {
   TMessage,
   TStreamState,
   TToolCall,
-  TToolResult,
 } from "@/data-provider/types";
 import MessageItem from "./message-item";
-import { useParams } from "next/navigation";
-import { ReactNode, useEffect, useRef, useState } from "react";
-import Spinner from "@/components/ui/spinner";
+import { useEffect, useRef } from "react";
 import { useChatMessages } from "@/hooks/useChat";
 import ToolContainer from "../../tools/tool-container";
 import { ToolResult } from "../../tools/tool-result";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   streamingMessage?: TMessage | null;
@@ -33,7 +31,7 @@ export default function MessagesContainer({
   streamingMessage,
   onRetry,
   threadId,
-  stream
+  stream,
 }: Props) {
   const { messages } = useChatMessages(threadId, stream);
   const prevMessages = usePrevious(messages);
@@ -54,32 +52,36 @@ export default function MessagesContainer({
   }, [messages]);
 
   return (
-      <div className="p-6 overflow-y-scroll" ref={divRef}>
-        {messages?.map((message, index) => {
-          const isToolCall =
-            message.tool_calls?.length && message.tool_calls.length > 0;
+    <div className="p-6 overflow-y-scroll" ref={divRef}>
+      {messages?.map((message, index) => {
+        const isToolCall =
+          message.tool_calls?.length && message.tool_calls.length > 0;
 
-          const isToolResult = message.type === MessageType.TOOL;
+        const isToolResult = message.type === MessageType.TOOL;
 
-          if (isToolResult) {
-            return (
-              <ToolResult toolResult={message} key={`${message.id}-${index}`} />
-            );
-          }
+        const isLastMessage = index === messages.length - 1;
+        const isStopped = stream?.status === 'stopped'
 
-          if (isToolCall) {
-            return (
-              <ToolContainer
-                toolCalls={message.tool_calls as TToolCall[]}
-                key={`${message.id}-${index}`}
-              />
-            );
-          }
-
+        if (isToolResult) {
           return (
-            <MessageItem message={message} key={`${message.id}-${index}`} />
+            <ToolResult toolResult={message} key={`${message.id}-${index}`} />
           );
-        })}
-      </div>
+        }
+
+        if (isToolCall) {
+          return (
+            <ToolContainer
+              toolCalls={message.tool_calls as TToolCall[]}
+              key={`${message.id}-${index}`}
+            />
+          );
+        }
+
+        return <>
+          <MessageItem message={message} key={`${message.id}-${index}`} />
+          {isStopped && isLastMessage && <Button onClick={onRetry}>Retry</Button>}
+          </>;
+      })}
+    </div>
   );
 }
