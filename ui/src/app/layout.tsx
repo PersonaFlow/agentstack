873ed1eec2dbfd7cd36e98c029756f35cc26df89
Kris Hokr <kristoferhokr@gmail.com>
2024-08-30 17:35:33 -0600

    added favicon

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -6,15 +6,15 @@ import { Toaster } from "@/components/ui/toaster";
 import BuildPanel from "@/components/features/build-panel/components/build-panel";
 import Navbar from "@/components/features/navbar/components/navbar";
 import Header from "@/components/features/header/components/header";
-import { useState } from "react";
-import { SquareIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PersonaFlow",
   description: "Where AI meets individuality for unmatched personalization",
+  icons: {
+    icon: '/icon.ico'
+  }
 };
 
 export default function RootLayout({
