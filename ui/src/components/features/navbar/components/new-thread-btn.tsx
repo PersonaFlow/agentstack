071d0260e955430c3b991d87237dc439d561d378
Kris Hokr <kristoferhokr@gmail.com>
2024-09-15 10:41:01 -0600

    updated bg-secondary and secondary btn variant

--- ui/src/components/features/navbar/components/new-thread-btn.tsx
+++ ui/src/components/features/navbar/components/new-thread-btn.tsx
@@ -8,7 +8,7 @@ type TNewThreadBtnProps = {
 
 export default function NewThreadBtn({ handleClick, disabled }: TNewThreadBtnProps) {
   return (
-    <Button variant="outline" onClick={handleClick} className="p-6 w-[calc(100%-20px)] text-black" disabled={disabled}>
+    <Button variant="secondary" onClick={handleClick} className="p-6 w-[calc(100%-20px)]" disabled={disabled}>
       <a className="flex gap-x-2 items-center">
         <PlusSquareIcon />
         <span>New thread</span>
