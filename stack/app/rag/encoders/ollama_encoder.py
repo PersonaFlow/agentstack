bcc17d64e496ec4f21a6c3090bdc1554854f8ff9
Dan Orlando <daorlando@webmd.net>
2024-10-10 11:34:13 -0700

    fix ollama encoder

--- stack/app/rag/encoders/ollama_encoder.py
+++ stack/app/rag/encoders/ollama_encoder.py
@@ -1,24 +1,29 @@
 from typing import List
-
+from pydantic import Field
 from semantic_router.encoders import BaseEncoder
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-
+from langchain_community.embeddings import OllamaEmbeddings
 from stack.app.core.configuration import get_settings
 
 settings = get_settings()
 
-
-def get_ollama_embeddings():
-    return OllamaEmbeddings(
-        model=settings.VECTOR_DB_ENCODER_MODEL, base_url=settings.OLLAMA_BASE_URL
-    )
-
-
 class OllamaEncoder(BaseEncoder):
-    name = "OllamaEncoder"
-    score_threshold = 0.5
-    type = "ollama"
-    embeddings = get_ollama_embeddings()
+    name: str = Field(default="all-minilm")
+    score_threshold: float = Field(default=0.75)
+    type: str = Field(default="ollama")
+    dimensions: int = Field(default=384)
+    embeddings: OllamaEmbeddings = Field(default=None)
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if 'name' in data:
+            self.embeddings = OllamaEmbeddings(
+                model=self.name,
+                base_url=settings.OLLAMA_BASE_URL
+            )
 
     def __call__(self, docs: List[str]) -> List[List[float]]:
         return self.embeddings.embed_documents(docs)
+
