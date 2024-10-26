bdadfe0feb542b8a70081d20b8a356aa0d6d949c
Dan Orlando <daorlando@webmd.net>
2024-10-26 14:45:21 -0700

    Make input optional for when chat is resumed

--- stack/app/api/v1/runs.py
+++ stack/app/api/v1/runs.py
@@ -61,7 +61,7 @@ class CreateRunPayload(BaseModel):
 
     assistant_id: Optional[str] = None
     thread_id: Optional[str] = None
-    input: list[dict]
+    input: Optional[list[dict]] = None
     config: Optional[RunnableConfig] = None
 
 
