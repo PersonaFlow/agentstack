025568985d2ee3aeb8035b61752a4749a8ac157e
Stas A <alstas@gmail.com>
2024-11-12 21:41:40 -0800

    queries. do not refetch on window focus

--- ui/src/providers/Providers.tsx
+++ ui/src/providers/Providers.tsx
@@ -10,6 +10,7 @@ export default function Providers({ children }: { children: ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
+            refetchOnWindowFocus: false,
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
             // staleTime: 60 * 1000,
